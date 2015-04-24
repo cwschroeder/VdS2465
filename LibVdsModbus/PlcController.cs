a517ccfea114e4812f20b9bb31c231e5a4aa1623
cwschroeder <christian.w.schroeder@gmail.com>
2015-04-24 20:18:23 +0200

    Fixed bug with missing data at measurement frame creation

--- LibVdsModbus/PlcController.cs
+++ LibVdsModbus/PlcController.cs
@@ -132,21 +132,21 @@ namespace LibVdsModbus
         {
             Log.Debug("Create change message VOLTAGE with value {0}", voltage);
             //return FrameVdS_30.Create(0x01, 0x00, 0x00, 0x00, 0x21, voltage);
-            return FrameVdS_54.Create(string.Format("Spannung geändert, neuer Wert: {0} V"));
+            return FrameVdS_54.Create(string.Format("Spannung geändert, neuer Wert: {0} V", voltage));
         }
 
         public static FrameVdS CreateMeasurementMessageCurrent(double current)
         {
             Log.Debug("Create change message CURRENT with value {0}", current);
             //return FrameVdS_30.Create(0x01, 0x00, 0x00, 0x00, 0x31, current);
-            return FrameVdS_54.Create(string.Format("Strom geändert, neuer Wert: {0} A"));
+            return FrameVdS_54.Create(string.Format("Strom geändert, neuer Wert: {0} A", current));
         }
 
         public static FrameVdS CreateMeasurementMessagePower(double power)
         {
             Log.Debug("Create change message POWER with value {0}", power);
             //return FrameVdS_30.Create(0x01, 0x00, 0x00, 0x00, 0x48, power);
-            return FrameVdS_54.Create(string.Format("Leistung geändert, neuer Wert: {0} kW"));
+            return FrameVdS_54.Create(string.Format("Leistung geändert, neuer Wert: {0} kW", power));
         }
     }
 }
